e326b4ce7cff1488077f0e5a0f9d7223a5adb971
hosanasoares <146008283+hosanasoares@users.noreply.github.com>
2024-03-13 12:45:41 -0300

    Update q4.py

--- nana pest/PEST-questoes/q4.py
+++ nana pest/PEST-questoes/q4.py
@@ -1,36 +1,21 @@
-def fatorial(num : int):
-    for i in range(1,num+1):
-        f *=i
-
-    return f
-
-
-
-
-def comb(bino : int):
-    global num
- 
-
-     
+def factorial(n : int ):
+    fact = 1
+    for n in range(1, n + 1):
+        fact *= n 
     
+    return fact
+def comb(n : int, p : int):
+    coe_b = factorial(n) / (factorial(p) * factorial(n - p))
+    return coe_b
 
-    
+n = int(input('Digite um número: '))
+p = int(input('Digite outro número: '))
+
+while n < 0:
+    n = int(input('Digite um outro número: '))
+while p < 0:
+    p = int(input('Digite um outro número: '))
 
 
-
-
-num = int(input('digite um número: '))
-
-while num < 0  :
-    num = int(input('digite um número: '))
-
-res = fatorial(num)
-print('o fatorial é :', res)
-
-bino = int(input('digite um número para binominal: '))
-
-while bino < 0:
-    bino = int(input('digite um número para binominal: '))
-
-res = comb(bino)
-print(res)
+resp = comb(n,p)
+print(resp)
