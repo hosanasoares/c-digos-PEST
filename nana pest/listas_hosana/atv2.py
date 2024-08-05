ae902b62fe18c99033d2c9853b86019ec1c57221
hosanasoares <146008283+hosanasoares@users.noreply.github.com>
2024-08-05 15:47:57 -0300

    Update atv2.py

--- nana pest/listas_hosana/atv2.py
+++ nana pest/listas_hosana/atv2.py
@@ -1,1 +1,11 @@
-lista = []
+#maior número da lista
+
+lista = [1, 344, -15, 1989, 2007, 23.5, 12, 25, 67.3]
+
+maior_numero = -99999
+
+for item in lista:
+    if item > maior_numero:
+        maior_numero = item
+
+print(maior_numero)
